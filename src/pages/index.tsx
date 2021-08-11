f7ccddcd8e7477a6eae9a9d203312ee2e218ae19
Abui <adjiem31@gmail.com>
2021-08-12 00:53:45 +0700

    feat: add react-range

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import { useState } from 'react';
 import { Play } from 'react-bootstrap-icons';
 import ReactPlayer from 'react-player';
+import { Direction, Range } from 'react-range';
 
 import { RoundedButton } from '@/components/Button';
 import { TextField } from '@/components/Field';
@@ -9,8 +10,6 @@ import { TextField } from '@/components/Field';
 const Home: NextPage = () => {
   const [url, setUrl] = useState('https://www.youtube.com/watch?v=keOFZ3cZvGQ');
   const [isPlaying, setPlaying] = useState(false);
-  const [isPlaying2, setPlaying2] = useState(false);
-  const [vol, setVol] = useState(1);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-indigo-900">
@@ -19,20 +18,61 @@ const Home: NextPage = () => {
       <RoundedButton onClick={() => setPlaying((v) => !v)}>
         <Play />
       </RoundedButton>
+      <div className="absolute right-20 top-20">
+        <PlayerVert />
+      </div>
+    </div>
+  );
+};
 
+const PlayerVert = () => {
+  const [isPlaying, setPlaying] = useState(false);
+  const [vol, setVol] = useState([0]);
+  return (
+    <div className="flex flex-col items-center">
       <ReactPlayer
-        volume={vol}
+        volume={vol / 100}
         loop
         width={0}
         height={0}
         url="/mix-kit/rain-thunder.wav"
         controls
-        playing={isPlaying2}
+        playing={isPlaying}
       />
-      <RoundedButton onClick={() => setPlaying2((v) => !v)}>
+      <RoundedButton onClick={() => setPlaying((v) => !v)}>
         <Play />
       </RoundedButton>
-      <input type="range" onChange={(e) => setVol(Number.parseFloat(e.target.value) / 100)} />
+      <Range
+        direction={Direction.Up}
+        renderTrack={({ props, children }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '60px',
+              width: 12,
+              backgroundColor: '#c4c4c4',
+              borderRadius: 200,
+            }}
+          >
+            {children}
+          </div>
+        )}
+        renderThumb={({ props }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '12px',
+              width: '24px',
+              background: '#FFFFFF',
+              borderRadius: 10,
+            }}
+          />
+        )}
+        values={vol}
+        onChange={(values) => setVol(values)}
+      />
     </div>
   );
 };
